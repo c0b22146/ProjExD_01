31ed0b54aa2b817f0263f93cbc8a225491fb2ddd
seita <c0b22146af@edu.teu.ac.jp>
2023-06-13 15:24:49 +0900

    第3問

--- pygame_image.py
+++ pygame_image.py
@@ -8,7 +8,8 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     koukaton = pg.image.load("ex01/fig/3.png")
-    koukaton_flip = pg.transform.flip(koukaton,True,False)
+    koukaton = pg.transform.flip(koukaton,True,False)
+    koukaton = pg.transform.rotozoom(koukaton,10,1.0)
     tmr = 0
     while True:
         for event in pg.event.get():
