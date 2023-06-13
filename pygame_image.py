328d41c472bd8973afc1c5ad980bb941de2e89c0
seita <c0b22146af@edu.teu.ac.jp>
2023-06-13 16:39:06 +0900

    演習

--- pygame_image.py
+++ pygame_image.py
@@ -13,18 +13,32 @@ def main():
     koukaton_list = [koukaton,koukaton_1]
     tmr = 0
     x = 0
+    xx = 0
+    w = 0
+    j = 1
+    oo = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img,[1600-x,0])
+        screen.blit(pg.transform.flip(bg_img,True,False),[1600-xx,0])
         i = tmr % 2
-        screen.blit(koukaton_list[i],[300,200])
+        koukaton_2 = pg.transform.rotozoom(koukaton, w, 1.0)
+        screen.blit(koukaton_2,[300,200])
         pg.display.update()
         tmr += 1      
         x += 1
-        if (x == 1600):
-            x = 0
+        xx += 1
+        w += j
+        if (x == 1601):
+            x = -1600
+        if (xx == 3201):
+            xx = 0
+        if (w >= 10):
+            j = -1
+        elif(w <= 0):
+            j = 1
+
         clock.tick(10)
 
 
